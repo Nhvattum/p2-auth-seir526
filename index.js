43588190fc341f4822c572abd5adf59b887c0251
Nick Hvattum <nhvattum@gmail.com>
2020-06-30 14:46:08 -0700

    session middleware added

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 require('dotenv').config();
 const Express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
-// passport, and custom middleware, sequelize sessions
 const helmet = require('helmet');
 const session = require('express-session');
 const flash = require('flash');
+const passport = require('./config.ppConfig');
+const db = require('./models');
+// Want to add a liink to our custom middleware for isLoggedIn
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 // App Setup
@@ -16,6 +19,33 @@ app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 app.use(helmet());
+
+// create new instance of class Sequelize Store
+const sessionStore = new SequelizeStore({
+    db: db.sequelize,
+    expiration: 1000 * 60 * 30
+})
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true
+}))
+
+sessionStore.sync();
+
+// initialize passport and session info
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function(req, res, next) {
+    res.locals.alert = req.flash();
+    res.locals.currentUser = req.user;
+
+    next();
+})
 
 // ROUTES
 app.get('/', function(req, res) {
